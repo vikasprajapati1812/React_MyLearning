db9e631a057d313e95ebf86b8ec0bec5811f8157
vikasprajapati1812 <118847227+vikasprajapati1812@users.noreply.github.com>
2024-06-23 22:52:11 +0530

    Delete 4.0 PARCEL directory

--- 4.0 PARCEL/data.js
+++
@@ -1,9 +0,0 @@
-//two ways to export the object... if using variable then we dont have to write default else we have to write it.
-
-export default {
-    name:"vikas"
-}
-
-export const user= {
-    name:"vikas"
-}
