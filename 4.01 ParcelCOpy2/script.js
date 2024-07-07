7294994f6eb4fb15b6e1e17769b9a874a313478f
Vikas <vikasprajapati1812@gmail.com>
2024-07-07 21:56:23 +0530

    parcel second copy

---
+++ 4.01 ParcelCOpy2/script.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client"
+
+console.log("hello From the Scripts");
+
+const h2=<h1>Hello JSX</h1>
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(h2)
